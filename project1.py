4ea4a0f8ec6157410ec1e776bbc2e427f1850e5d
Santiago <m1rodrigues@ryerson.ca>
2024-10-20 01:37:40 -0400

    Loosing my mind over the models
    Finally can start testing some models yay

--- project1.py
+++ project1.py
@@ -8,6 +8,9 @@ Created on Thu Oct 17 21:09:15 2024
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import StandardScaler
 
 """
 Read data and convert to dataframe
@@ -40,11 +43,43 @@ for steps in range(1, 14):
 
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
-ax.set_zlabel('Z', )
+ax.set_zlabel('Z')
 ax.legend(title='Step', bbox_to_anchor=(1.05, 1))
 plt.show()
-# Note: This might qualify as data snooping bias as the data has not been
-#split between train and test yet, however, the project steps ask for data
-# visualization before data is prepared for the model and it is helpful
-# to at least know what the data in question represents.
+
+"""
+Correlation Analysis
+"""
+
+corr_matrix = df.corr(method='pearson')
+plt.figure()
+sns.heatmap(np.absolute(corr_matrix))
+plt.show()
+
+independent = df.drop('Step',axis=1)
+corr_matrix2 = independent.corr(method='pearson')
+plt.figure()
+sns.heatmap(np.absolute(corr_matrix2))
+plt.show()
+"""
+Classification Model Development
+"""
+#Splitting the data
+splitter1 = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in splitter1.split(df, df['Step']):
+    df_train = df.loc[train_index].reset_index(drop=True)
+    df_test = df.loc[test_index].reset_index(drop=True)
+
+#Variable selection
+X_train = df_train.drop("Step", axis=1)
+X_test = df_test.drop("Step", axis=1)
+y_train = df_train["Step"]
+y_test = df_test["Step"]
+
+#Scaling
+scaler1 = StandardScaler()
+X_train_scaled = pd.DataFrame(scaler1.fit_transform(X_train), columns=X_train.columns, index=X_train.index)
+X_test_scaled = pd.DataFrame(scaler1.transform(X_test), columns=X_test.columns, index=X_test.index)
+
+
 
